846d1de11423a80aa823403acf676b982ea29ad5
Stephan Griesel <sgriesel@gmail.com>
2018-09-03 12:18:47 +0200

    start context component

---
+++ src/context.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Provider extends Component {
+  render() {
+    return (
+      <Context.Provider>
+          
+      </Context.Provider>
+    )
+  }
+}
