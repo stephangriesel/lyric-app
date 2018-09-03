5725eb37e5e4cca1ce6e2b12d3fa2387a513e611
Stephan Griesel <sgriesel@gmail.com>
2018-09-03 11:51:42 +0200

    add tunes component

---
+++ src/components/layout/Index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Tunes from '../tunes/Tunes';
+
+const Index = () => {
+  return (
+    <React.Fragment>
+        <Tunes />
+    </React.Fragment>
+  );
+};
+
+
+export default Index;
---
+++ src/components/tunes/Tunes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class Tunes extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Tunes</h1>
+      </div>
+    )
+  }
+}
+
+export default Tunes;
