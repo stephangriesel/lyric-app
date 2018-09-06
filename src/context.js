6da6d2c6a7ef3441f01a255ff2a55e9e22e7f16d
Stephan Griesel <sgriesel@gmail.com>
2018-09-06 06:38:56 +0200

    test api

--- src/context.js
+++ src/context.js
@@ -12,7 +12,7 @@ export class Provider extends Component {
   };
 
 componentDidMount() {
-  axios.get()
+  axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=15&country=nl&f_has_lyrics=1&apikey=${process.env.REACT_APP_MY_KEY}`)
   .then(res => console.log(res.data))
   .catch(err => console.log(err));
 }
