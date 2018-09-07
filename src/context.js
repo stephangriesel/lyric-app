71ebdf50654edfe287cb3eca214220bbb61426a9
Stephan Griesel <sgriesel@gmail.com>
2018-09-07 05:40:19 +0200

    test tunes list in state

--- src/context.js
+++ src/context.js
@@ -12,8 +12,11 @@ export class Provider extends Component {
   };
 
 componentDidMount() {
-  axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=15&country=nl&f_has_lyrics=1&apikey=${process.env.REACT_APP_MY_KEY}`)
-  .then(res => console.log(res.data))
+  axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=20&country=nl&f_has_lyrics=1&apikey=${process.env.REACT_APP_MY_KEY}`)
+  .then(res => {
+    // console.log(res.data);
+    this.setState({tunes_list: res.data.message.body.track_list});
+  })
   .catch(err => console.log(err));
 }
 
