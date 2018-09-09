f8bb3b0968b6e970b5ae7800c5a42d2c296397d7
Stephan Griesel <sgriesel@gmail.com>
2018-09-09 08:22:25 +0200

    set state for lyrics

--- src/components/tunes/Lyrics.js
+++ src/components/tunes/Lyrics.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class Lyrics extends Component {
+    
+    state = {
+        track: {},
+        lyrics: {}
+    };
+
+    componentDidMount() {
+        axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MY_KEY}`)
+  .then(res => {
+    console.log(res.data);
+    // this.setState({tunes_list: res.data.message.body.track_list});
+  })
+  .catch(err => console.log(err));
+}
+    
+
   render() {
     return (
       <div>
