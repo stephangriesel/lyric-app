e5c000da7beb33fd3f16685a2e94f905c6db9d86
Stephan Griesel <sgriesel@gmail.com>
2018-09-09 07:53:01 +0200

    more styling & add additional details

--- src/components/tunes/Tune.js
+++ src/components/tunes/Tune.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Tune = props => {
   const { tune } = props;
@@ -7,6 +8,15 @@ const Tune = props => {
       <div className="card mb-4 shadow-sm">
         <div className="card-body">
           <h5>{tune.artist_name}</h5>
+          <p className="card-text">
+          <strong><i className="fas fa-play"></i> Tune</strong>: {tune.track_name}
+          <br />
+          <strong><i className="fas fa-compact-disc"></i> Album</strong>: {tune.album_name}
+          </p>
+          <Link to={`lyrics/track/${tune.track_id}`} className="btn btn-light btn-block">
+          <i className="fas fa-chevron-right"></i> Lyrics
+          
+          </Link>
         </div>
       </div>
     </div>
