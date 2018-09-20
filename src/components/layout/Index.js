71f6baaf07e73f76d7cb1ad150b451511e6c91e1
Stephan Griesel <sgriesel@gmail.com>
2018-09-20 05:47:13 +0200

    start with search component

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Tunes from '../tunes/Tunes';
+import Search from '../tunes/Search';
+
 
 const Index = () => {
   return (
     <React.Fragment>
+      <Search />
         <Tunes />
     </React.Fragment>
   );
--- src/components/tunes/Search.js
+++ src/components/tunes/Search.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Consumer } from '../../context';
+import React, { Component } from "react";
+import axios from "axios";
+import { Consumer } from "../../context";
 
 class Search extends Component {
-    state = {
-        trackTitle: ''
-    }
+  state = {
+    trackTitle: ""
+  };
   render() {
     return (
       <Consumer>
-          {value => {
-              return (
-                  <div className="card card-body mb-4 p-4">
+        {value => {
+          return (
+            <div className="card card-body mb-4 p-4">
+              <h1 className="display-5 text-center">
+                <i className="fas fa-music" />
+                Search for my tune
+              </h1>
+              <p className="lead text-center">Sing a long.. find the lyrics</p>
+              <form>
+                  <div className="form-group">
+                  <input type="text" className="form-control form-control-lg" placeholder="Tell me what you want what you really really want.." name="trackTitle" value={this.state.trackTitle}></input>
                   </div>
-              )
-          }}
+              </form>
+            </div>
+          );
+        }}
       </Consumer>
-    )
+    );
   }
 }
 
-export default Search
+export default Search;
