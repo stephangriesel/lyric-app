b51e3236f2cf9ccf1b0e2f72698edd79466dbc0f
Stephan Griesel <sgriesel@gmail.com>
2018-09-23 09:14:17 +0200

    remove genre... for now

--- src/components/tunes/Lyrics.js
+++ src/components/tunes/Lyrics.js
@@ -67,9 +67,9 @@ class Lyrics extends Component {
               {/* Fix date with npm package: moment & react-moment*/}
                   <strong>Release Year</strong>: <Moment format="YYYY">{track.first_release_date}</Moment>
               </li>
-              <li className="list-group-item">
+              {/*<li className="list-group-item">
                   <strong>Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
-              </li>
+      </li>*/}
               <li className="list-group-item">
               {/* Ternary Operator:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator */}
               <strong>Explicit: </strong>{track.explicit === 0 ? 'Nope' : 'Yep'}
